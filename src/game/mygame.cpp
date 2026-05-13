59c217df93320e8d94739a6ff81953b4b7a51ce0
Alexfire228 <adobe70980976@gmail.com>
2026-05-13 18:41:20 +0400

    13.05.26
    Initial commit #2

--- src/game/mygame.cpp
+++ src/game/mygame.cpp
@@ -10,30 +10,7 @@ void MyGame::Init(Renderer& renderer)
 
 void MyGame::Update()
 {
-    if (Input::IsHeld(PSP_CTRL_LEFT))
-        playerX -= playerSpeed;
-
-    if (Input::IsHeld(PSP_CTRL_RIGHT))
-        playerX += playerSpeed;
-
-    if (Input::IsHeld(PSP_CTRL_UP))
-        playerY -= playerSpeed;
-
-    if (Input::IsHeld(PSP_CTRL_DOWN))
-        playerY += playerSpeed;
-
-    // ограничение экрана
-    if (playerX < 0)
-        playerX = 0;
-
-    if (playerY < 0)
-        playerY = 0;
-
-    if (playerX > 480 - 64)
-        playerX = 480 - 64;
-
-    if (playerY > 272 - 64)
-        playerY = 272 - 64;
+    
 }
 
 void MyGame::Render(Renderer& renderer)
@@ -42,9 +19,9 @@ void MyGame::Render(Renderer& renderer)
         playerTexture,
         64,
         64,
-        playerX,
-        playerY
-    );
+        208,
+        100
+        );
 }
 
 void MyGame::Shutdown()
