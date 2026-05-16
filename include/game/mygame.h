5c7e1714a7d19dde0f49406271282e0f665b3b89
Alexfire228 <adobe70980976@gmail.com>
2026-05-16 15:19:33 +0400

    remove player code from mygame.h

--- include/game/mygame.h
+++ include/game/mygame.h
@@ -9,12 +9,4 @@ public:
     void Update() override;
     void Render(Renderer& renderer) override;
     void Shutdown() override;
-
-private:
-    unsigned int* playerTexture = nullptr;
-
-    float playerX = 208.0f;
-    float playerY = 104.0f;
-
-    float playerSpeed = 2.0f;
 };
